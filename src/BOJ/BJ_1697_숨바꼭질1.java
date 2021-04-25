65025f545849728505f2b8dcac7913e092c0d70b
dokyeong10 <dign1571@naver.com>
2021-04-25 14:58:38 +0900

    0425

--- src/BOJ/BJ_1697_숨바꼭질1.java
+++ src/BOJ/BJ_1697_숨바꼭질1.java
@@ -6,48 +6,40 @@ import java.util.Scanner;
 
 public class BJ_1697_숨바꼭질1 {
 
-    static int subin, sister, cnt = 0;
-    static int map[];
+    static int subin, sister;
+    static int map[]= new int[100001]; // 범위가 <=100000 이다
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         subin = sc.nextInt();
         sister = sc.nextInt();
-        map = new int[100001];
-
         bfs();
-        System.out.println(map[sister]);
-
-
     }// end of main
 
     private static void bfs() {
         Queue<Integer> q = new LinkedList<>();
         q.offer(subin);
+        map[subin] = 1;
 
         while (!q.isEmpty()) {
             subin = q.poll();
             if (subin == sister) {
                 break;
             }
+            if (subin + 1 <= 100000 && map[subin + 1] == 0) {
+                q.offer(subin + 1);
+                map[subin + 1] = map[subin] + 1;
+            }
             if (subin - 1 >= 0 && map[subin - 1] == 0) {
                 q.offer(subin - 1);
                 map[subin - 1] = map[subin] + 1;
+            }
 
-            }
-            if (subin + 1 <= 100000 && map[subin + 1] == 0) {
-                q.offer(subin + 1);
-                map[subin - 1] = map[subin] + 1;
-            }
             if (subin * 2 <= 100000 && map[subin * 2] == 0) {
                 q.offer(subin * 2);
                 map[subin * 2] = map[subin] + 1;
             }
-
-
         }
-
-
+        System.out.println(map[sister] - 1); // 처음에 1로 시작해서 1빼줌
     }
 }  // end of class

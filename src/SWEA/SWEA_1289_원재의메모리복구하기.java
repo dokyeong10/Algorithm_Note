d455f1931fc834c2f834003c99ab7052c878b14e
dokyeong10 <dign1571@naver.com>
2021-04-28 23:14:04 +0900

    0428

--- src/SWEA/SWEA_1289_원재의메모리복구하기.java
+++ src/SWEA/SWEA_1289_원재의메모리복구하기.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 public class SWEA_1289_원재의메모리복구하기 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
 
-        int TC =sc.nextInt();
-        for (int test_case = 1; test_case <=TC; test_case++) {
-            String s =sc.next();
-            int []bit =new int[s.length()];
+        int TC = sc.nextInt();
+        for (int test_case = 1; test_case <= TC; test_case++) {
+            String s = sc.next();
+            int[] bit = new int[s.length()];
 
             for (int i = 0; i < s.length(); i++) {
-                bit[i]=s.charAt(i)-'0';
+                bit[i] = s.charAt(i) - '0';
             } // 입력 후 쪼개기 완료 //
 
             for (int i = 0; i < s.length(); i++) {
                 //if()
             }
-int result =0;
+            int result = 0;
+            // ㅋㅓ밋
 
 
-
-        sb.append("#").append(test_case).append(" ").append("");
+            sb.append("#").append(test_case).append(" ").append("");
         } //end of tc
         System.out.println(sb);
 

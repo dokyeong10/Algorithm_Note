797458c08ff5befc37f68a0e8104486fd44f2a8c
dokyeong10 <dign1571@naver.com>
2021-06-07 21:04:09 +0900

    0607

---
+++ src/BOJ/BJ_1713_후보추천하기.java
@@ -0,0 +1,20 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BJ_1713_후보추천하기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int reco = sc.nextInt();
+        int num[] = new int[reco];
+        int poto[] = new int[reco + 1];
+
+        for (int i = 0; i < reco; i++) {
+            num[i] = sc.nextInt();
+        } // 입력 받기 완료
+
+
+    }// end of main
+} // end of class

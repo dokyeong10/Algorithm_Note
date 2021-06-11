2186c8b6195107e5e375348c7c6d1269fc1d0b33
dokyeong10 <dign1571@naver.com>
2021-06-12 02:06:24 +0900

    0612

--- src/BOJ/BJ_1343_폴리오미노.java
+++ src/BOJ/BJ_1343_폴리오미노.java
@@ -1,4 +1,61 @@
 package BOJ;
 
+import java.util.Scanner;
+
 public class BJ_1343_폴리오미노 {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char[] X = new char[s.length()];
+        int cnt = 0;
+        String result = "";
+        if (s.equals("X")) {
+            System.out.println(-1);
+            return;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            X[i] = s.charAt(i);
+            if (X[i] == 'X') {
+                cnt++;
+                if (i == s.length() - 1) {
+                    while (cnt > 0) {
+                        if (cnt >= 4) {
+                            result += "AAAA";
+                            cnt -= 4;
+                        } else {
+                            result += "BB";
+                            cnt -= 2;
+                        }
+                    }
+                }
+            } else if (X[i] == '.' && cnt % 2 != 0) {
+                System.out.println(-1);
+                return;
+            } else if (X[i] == '.' && cnt == 0) {
+                result += ".";
+                cnt = 0;
+            } else if (X[i] == '.' && cnt % 2 == 0) {
+                while (cnt > 0) {
+                    if (cnt >= 4) {
+                        result += "AAAA";
+                        cnt -= 4;
+                        if (cnt == 0) {
+                            result += '.';
+                        }
+                    } else {
+                        result += "BB";
+                        cnt -= 2;
+                        if (cnt == 0) {
+                            result += '.';
+                        }
+                    }
+                }
+            }
+        }
+        if (cnt != 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(result);
+        }
+    } // end of main
+} // end of class
---
+++ src/BOJ/BJ_2217_로프.java
@@ -0,0 +1,15 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BJ_2217_로프 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int N =sc.nextInt();
+        int rope[] =new int[N];
+        for (int i = 0; i < N; i++) {
+            rope[i]=sc.nextInt();
+        }
+
+    } // end of main
+} // end of class

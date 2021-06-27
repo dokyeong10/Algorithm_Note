1faba2b5d5af993b324bcfb49fe5bcfb739003ae
dokyeong10 <dign1571@naver.com>
2021-06-28 02:02:16 +0900

    0628

--- src/BOJ/BJ_2636_치즈.java
+++ src/BOJ/BJ_2636_치즈.java
@@ -15,7 +15,6 @@ public class BJ_2636_치즈 {
     static Queue<Point> q = new LinkedList<>();
     static boolean check[][];
 
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -35,33 +34,25 @@ public class BJ_2636_치즈 {
             }
         }/// 입력받기 완료
         int result = 0;
-
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (cheese[i][j] == 0 && (i == 0 || j == 0 || i == N - 1 || j == M - 1) && !check[i][j]) {
                     check[i][j] = true;
                     cheese[i][j] = 2;
                     q.offer(new Point(i, j));
-
                 }
             }
         }
         BFS_Ze();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                System.out.print(cheese[i][j] + " ");
+                if (check[i][j]) check[i][j] = false;
             }
-            System.out.println();
         }
-        System.out.println();
-
-        while (ch_num > 0) {
-            result++;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (check[i][j]) check[i][j] = false;
-                }
-            }
+        int last_cnt = 0;
+        while (ch_num != 0) {
+            //erase
+            int cnt = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
                     if (cheese[i][j] == 1) {
@@ -71,29 +62,60 @@ public class BJ_2636_치즈 {
                             nj = j + dc[d];
                             if (ni < 0 || nj < 0 || ni > N - 1 || nj > M - 1) continue;
                             if (check[ni][nj]) continue;
+                            if (check[i][j]) continue;
                             if (cheese[ni][nj] == 2) {
                                 cheese[i][j] = 2;
                                 check[i][j] = true;
+                                q.offer(new Point(i, j));
                                 ch_num--;
+                                cnt++;
                             }
                         }
                     }
                 }
             }
+            last_cnt = cnt;
+            cnt = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
-                    System.out.print(cheese[i][j]+" ");
+                    if (check[i][j]) check[i][j] = false;
                 }
-                System.out.println();
             }
-            System.out.println();
-
-
+            // find cheese
+            BFS();
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (check[i][j]) check[i][j] = false;
+                }
+            }
+            result++;
         }
         System.out.println(result);
-
+        System.out.println(last_cnt);
 
     } // end of main
+
+    private static void BFS() {
+        int r, c;
+        while (!q.isEmpty()) {
+            Point cur = q.poll();
+            r = cur.r;
+            c = cur.c;
+            check[r][c] = true;
+            int nr, nc;
+            for (int d = 0; d < 4; d++) {
+                nr = r + dr[d];
+                nc = c + dc[d];
+                if (nr < 0 || nc < 0 || nr > N - 1 || nc > M - 1) continue;
+                if (check[nr][nc]) continue;
+                if (cheese[nr][nc] == 0) {
+                    cheese[nr][nc] = 2;
+                    check[nr][nc] = true;
+                    q.offer(new Point(nr, nc));
+                }
+            }
+        }
+    }
 
     private static void BFS_Ze() {
         while (!q.isEmpty()) {

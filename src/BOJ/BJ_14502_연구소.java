2bbcc4bc1eda7f27b1fc42ecd3028f733c11b754
dokyeong10 <dign1571@naver.com>
2021-07-04 02:17:46 +0900

    0704

---
+++ src/BOJ/BJ_14502_연구소.java
@@ -0,0 +1,4 @@
+package BOJ;
+
+public class BJ_14502_연구소 {
+}
---
+++ src/programmers/오픈채팅방.java
@@ -0,0 +1,35 @@
+package programmers;
+
+import java.util.*;
+import java.io.*;
+
+public class 오픈채팅방 {
+    public String[] solution(String[] record) {
+        StringBuilder sb = new StringBuilder();
+        HashMap<String, String> map = new HashMap<>();
+        ArrayList<String[]> list = new ArrayList<>();
+        for (int i = 0; i < record.length; i++) {
+            String rel[] = record[i].split(" ");
+            if (rel[0].equals("Enter")) {
+                map.put(rel[1], rel[2]);
+                String arr[] = new String[2];
+                arr[0] = rel[1];
+                arr[1] = "님이 들어왔습니다.";
+                list.add(arr);
+            } else if (rel[0].equals("Leave")) {
+                map.get(rel[1]);
+                String arr[] = new String[2];
+                arr[0] = rel[1];
+                arr[1] = "님이 나갔습니다.";
+                list.add(arr);
+            } else if (rel[0].equals("Change")) {
+                map.put(rel[1], rel[2]);
+            }
+        }
+        String[] answer = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = map.get(list.get(i)[0]) + list.get(i)[1];
+        }
+        return answer;
+    }
+}

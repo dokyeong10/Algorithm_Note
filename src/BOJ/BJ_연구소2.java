f446ccc8bb8cd92e72061c271a55b173d73e3a29
dokyeong10 <dign1571@naver.com>
2021-07-24 22:13:09 +0900

    숫자문자열과영단어

--- src/BOJ/BJ_연구소2.java
+++ src/BOJ/BJ_연구소2.java
@@ -1,4 +1,8 @@
 package BOJ;
 
 public class BJ_연구소2 {
+    public static void main(String[] args) {
+        String s ="abdbdb";
+        System.out.println(s.substring(1,5));
+    }
 }
---
+++ src/ETC1/숫자문자열과영단어.java
@@ -0,0 +1,47 @@
+package ETC1;
+
+import java.util.*;
+
+public class 숫자문자열과영단어 {
+    public int solution(String s) {
+        int answer = 0;
+        String tmp = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                tmp += s.charAt(i);
+            } else if (s.length() > i + 3 && s.substring(i, i + 4).equals("zero")) {
+                tmp += '0';
+                i += 3;
+            } else if (s.length() > i + 2 && s.substring(i, i + 3).equals("one")) {
+                tmp += '1';
+                i += 2;
+            } else if (s.length() > i + 2 && s.substring(i, i + 3).equals("two")) {
+                tmp += '2';
+                i += 2;
+            } else if (s.length() > i + 4 && s.substring(i, i + 5).equals("three")) {
+                tmp += '3';
+                i += 4;
+            } else if (s.length() > i + 3 && s.substring(i, i + 4).equals("four")) {
+                tmp += '4';
+                i += 3;
+            } else if (s.length() > i + 3 && s.substring(i, i + 4).equals("five")) {
+                tmp += '5';
+                i += 3;
+            } else if (s.length() > i + 2 && s.substring(i, i + 3).equals("six")) {
+                tmp += '6';
+                i += 2;
+            } else if (s.length() > i + 4 && s.substring(i, i + 5).equals("seven")) {
+                tmp += '7';
+                i += 4;
+            } else if (s.length() > i + 4 && s.substring(i, i + 5).equals("eight")) {
+                tmp += '8';
+                i += 4;
+            } else if (s.length() > i + 3 && s.substring(i, i + 4).equals("nine")) {
+                tmp += '9';
+                i += 3;
+            }
+        }
+        answer = Integer.valueOf(tmp);
+        return answer;
+    }
+}

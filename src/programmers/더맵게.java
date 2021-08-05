b7ec2d5f934ed8987b69ce102dacb221323677f6
dokyeong10 <dign1571@naver.com>
2021-08-05 23:34:55 +0900

    re 더맵게

--- src/programmers/더맵게.java
+++ src/programmers/더맵게.java
@@ -1,7 +1,10 @@
 package programmers;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class 더맵게 {
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }

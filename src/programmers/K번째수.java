bd5a3d8bcf27868c924df3aeb54eae319a385cad
dokyeong10 <dign1571@naver.com>
2021-08-06 23:27:03 +0900

    0806

--- src/programmers/K번째수.java
+++ src/programmers/K번째수.java
@@ -2,7 +2,7 @@ package programmers;
 
 import java.util.*;
 
-public class K번째수 {
+public class K번째수 { //k
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         for (int i = 0; i < commands.length; i++) {

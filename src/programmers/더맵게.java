a9e2d062af80876a8ae594d191af9b40e254e18f
dokyeong10 <dign1571@naver.com>
2021-09-06 22:26:20 +0900

    feat : 우선순위 큐

--- src/programmers/더맵게.java
+++ src/programmers/더맵게.java
@@ -5,21 +5,19 @@ import java.util.*;
 public class 더맵게 { //우선 순위 큐
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < scoville.length; i++) {
+        PriorityQueue<Integer>pq = new PriorityQueue<>();
+        for(int i=0; i<scoville.length; i++){
             pq.offer(scoville[i]);
         }
-        while (pq.size() > 1) {
-            if (pq.peek() >= K) {
+        while(pq.size()>1){
+            if(pq.peek()>=K){
                 return answer;
-            } else {
+            }else{
                 answer++;
-                int first = pq.peek();
-                pq.remove();
-                int sec = pq.peek();
-                pq.remove();
-                pq.offer(first + (sec * 2));
-                if (pq.peek() >= K) {
+                int first = pq.poll();
+                int sec = pq.poll();
+                pq.offer(first+(sec*2));
+                if(pq.peek()>=K){
                     return answer;
                 }
             }

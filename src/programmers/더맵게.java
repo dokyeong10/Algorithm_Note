81da5b950d72edada22d8c3ab1d60a37ba07a747
dokyeong10 <dign1571@naver.com>
2021-09-06 22:21:53 +0900

    feat : 우선순위 큐

--- src/programmers/더맵게.java
+++ src/programmers/더맵게.java
@@ -1,10 +1,30 @@
 package programmers;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
 
 public class 더맵게 { //우선 순위 큐
-    public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < scoville.length; i++) {
+            pq.offer(scoville[i]);
+        }
+        while (pq.size() > 1) {
+            if (pq.peek() >= K) {
+                return answer;
+            } else {
+                answer++;
+                int first = pq.peek();
+                pq.remove();
+                int sec = pq.peek();
+                pq.remove();
+                pq.offer(first + (sec * 2));
+                if (pq.peek() >= K) {
+                    return answer;
+                }
+            }
+        }
+        answer = -1;
+        return answer;
     }
 }

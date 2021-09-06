9ff79a4aa6df921e29414115c5b5ae3b64094d32
dokyeong10 <dign1571@naver.com>
2021-09-06 22:01:07 +0900

    feat : 우선순위 큐

--- src/programmers/더맵게.java
+++ src/programmers/더맵게.java
@@ -3,7 +3,7 @@ package programmers;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class 더맵게 {
+public class 더맵게 { //우선 순위 큐
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }

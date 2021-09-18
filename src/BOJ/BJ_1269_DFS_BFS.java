73282e6e37cb7855ce984c5f645d5f981bd77c6a
dokyeong10 <dign1571@naver.com>
2021-09-18 23:16:38 +0900

    feat : DFS, BFS

---
+++ src/BOJ/BJ_1269_DFS_BFS.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_1269_DFS_BFS { // dfs bfs
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken()); //정점의 개수
+        int M = Integer.parseInt(st.nextToken()); // 간선의 개수
+        int V = Integer.parseInt(st.nextToken()); // 시작 번호
+        
+
+
+
+
+    } // end of main
+} // end of class
